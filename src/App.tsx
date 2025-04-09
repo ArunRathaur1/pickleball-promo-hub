af335a664b3f7bf47faa0cecd7df0ff242a1ecbf
INDRESH VERMA <indreshkverma2004@gmail.com>
2025-04-09 08:10:31 +0530

    updated code

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ import Sponser from './components/sponser/Sponser.js'
 import TournamentDetails from "./components/tournaments/tournamentDetails.tsx";
 import Clubdetails from "./components/userclubs/clubdetails.tsx";
 import { BlogDetail } from "./components/blogs/BlogDetail.tsx";
+import ClubForm from "./components/admin-club/form.tsx";
 
 const queryClient = new QueryClient();
 
@@ -90,6 +91,7 @@ const App = () => {
             <Route path="/courts" element={<CourtPage />} />
             <Route path="/admin/dashboard" element={<AdminDashboard />} />
             <Route path="/dashboard" element={<UserDashboard />} />
+            <Route path="/addclubs" element={<ClubForm/>} />
             <Route path="/playerprofile/:id" element={<PlayerProfile />} />
             <Route path="/signup" element={<Register></Register>}></Route>
             <Route path="/tournament/:id" element={<TournamentDetails />} />
